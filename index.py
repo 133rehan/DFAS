4989e29a1c0aa9b335b6743e0f7a5857d7ff593c
133rehan <115294531+133rehan@users.noreply.github.com>
2024-06-28 01:07:23 +0500

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,6 @@ import librosa
 import matplotlib.pyplot as plt
 from moviepy.editor import VideoFileClip
 
-
 # Loading model
 models_path = r"models/"
 xgboost_model = joblib.load(os.path.join(models_path, 'XG Boost.pkl'))
